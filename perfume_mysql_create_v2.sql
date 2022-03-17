b39330b9bc203e5d758ef75bf048f06f0018ce09
tranvando <75720596+tranvando@users.noreply.github.com>
2022-03-17 12:00:41 +0700

    update name product

--- perfume_mysql_create_v2.sql
+++ perfume_mysql_create_v2.sql
@@ -167,6 +167,8 @@ ALTER TABLE `user_role` ADD CONSTRAINT `user_role_fk1` FOREIGN KEY (`id_role`) R
 
 
 
+
+
 -- sửa kiểu dữ liệu
 ALTER TABLE product MODIFY COLUMN guarantee varchar(30);
 -- xóa cột
@@ -177,59 +179,61 @@ ALTER TABLE bill ADD COLUMN payment varchar(100);
 drop table news;
 
 
+
+
+
+
+
 -- insert data brand
 select * from brand;
-insert into brand(name,status) values('Gucci',true);
+insert into brand(name,status) values('Dolce Gabbana',true);
 insert into brand(name,status) values('Chanel',true);
-insert into brand(name,status) values('Luis vuiton',true);
-insert into brand(name,status) values('Chanel',true);
-insert into brand(name,status) values('Chanel',true);
-insert into brand(name,status) values('Chanel',true);
-insert into brand(name,status) values('Chanel',true);
+insert into brand(name,status) values('Louis Vuitton',true);
+insert into brand(name,status) values('Test1',true);
+insert into brand(name,status) values('Test2',true);
+insert into brand(name,status) values('Test3',true);
+insert into brand(name,status) values('Test4',true);
 
 
 
 
 -- insert data product
 select * from product;
-select * from product where gender='nu';
-
-
-insert into product(id_brand,name,origin,gender,fragrant,guarantee,price,image,short_description,detail_description,amount,status)
-values
-(1,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Nam','Hoa hồng','1 tháng',2500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true),
-(1,'Nước Hoa Versace Eros Man EDT 5ml','Pháp','Nam','Hoa hồng','1 tháng',1500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true),
-(1,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Nam','Hoa hồng','1 tháng',2500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true),
-(1,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Unisex','Hoa hồng','1 tháng',4500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true),
-(1,'Versace Eros Man EDT 5ml','Pháp','Nam','Hoa hồng','1 tháng',2500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true),
-(1,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Unisex','Hoa hồng','1 tháng',2500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true),
-(1,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Nam','Hoa hồng','1 tháng',2500000000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true),
-(1,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Unisex','Hoa hồng','1 tháng',5500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true),
-(1,'Nước Hoa  Versace Eros Man EDT 5ml','Pháp','Nam','Hoa hồng','1 tháng',2500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true),
-(1,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Nữ','Hoa hồng','1 tháng',250000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true),
-(1,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Nữ','Hoa hồng','1 tháng',6500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true),
-(1,'Nước Hoa Nam Chanel 5ml','Pháp','Nam','Hoa hồng','1 tháng',25000000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true),
-(1,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Nữ','Hoa hồng','1 tháng',7500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true),
-(1,'Nước Hoa  Versace Eros Man EDT 5ml','Pháp','Unisex','Hoa hồng','1 tháng',2500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true),
-(1,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Unisex','Hoa hồng','1 tháng',7500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true),
-(1,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Nữ','Hoa hồng','1 tháng',250000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,true)
-
-;
 
 insert into product(id_brand,name,origin,gender,fragrant,guarantee,price,image,short_description,detail_description,amount,created_date,status)
 values
-(3,'chanel pháp ','Pháp','Nam','Hoa hồng','1 tháng',100000000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-11-09',true);
+(1,'DOLCE GABBANA K EDP 100ML','Pháp','Nam','Hoa hồng','1 tháng',500000,'anh1.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-11-09',true),
+(1,'DOLCE GABBANA THE ONE FOR MEN EDT 150ML','Pháp','Nam','Hoa hồng','1 tháng',350000,'anh2.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-10-09',true),
+(1,'DOLCE GABBANA K','Pháp','Nam','Hoa hồng','1 tháng',300000,'anh3.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-09-09',true),
+(1,'DOLCE GABBANA K EDP','Pháp','Nam','Hoa hồng','1 tháng',700000,'anh4.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2022-02-01',true),
+(1,'LIGHT BLUE EAU INTENSE WOMEN','Pháp','Nam','Hoa hồng','1 tháng',750000,'anh5.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-12-09',true),
+(1,'DOLCE ROSA EXCELCA EDP','Pháp','Nam','Hoa hồng','1 tháng',1000000,'anh6.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-07-09',true),
+(1,'THE ONE FOR MEN EDP','Pháp','Nam','Hoa hồng','1 tháng',1200000,'anh7.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-08-09',true),
+(1,'LIGHT BLUE POUR HOMME EDT','Pháp','Nam','Hoa hồng','1 tháng',1500000,'anh8.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-01-09',true),
+(1,'DOLCE SHINE','Pháp','Nam','Hoa hồng','1 tháng',2000000,'anh9.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-11-09',true),
+(1,'DOLCE GABBANA FLOWER EDP','Pháp','Nam','Hoa hồng','1 tháng',2200000,'anh10.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-11-09',true),
 
-insert into product(id_brand,name,origin,gender,fragrant,guarantee,price,image,short_description,detail_description,amount,created_date,status)
-values
-(3,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Unisex','Hoa hồng','1 tháng',500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-11-09',true),
-(3,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Unisex','Hoa hồng','1 tháng',25000000.00,'anh1,jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-12-09',true),
-(3,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Unisex','Hoa hồng','1 tháng',2500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-01-09',true),
-(3,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Nữ','Hoa hồng','1 tháng',2500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-11-09',true),
-(3,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Nữ','Hoa hồng','1 tháng',2500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-12-09',true),
-(3,'Nước Hoa Nam Versace Eros Man EDT 5ml','Pháp','Unisex','Hoa hồng','1 tháng',2500000.00,'anh1.jpg','Nước hoa rất ô xờ kê','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-01-09',true)
-;
+(2,'DOLCE GABBANA K EDP 100ML','Pháp','Nữ','Hoa hồng','1 tháng',500000,'anh1.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-11-09',true),
+(2,'DOLCE GABBANA THE ONE FOR MEN EDT 150ML','Pháp','Nữ','Hoa hồng','1 tháng',350000,'anh2.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-10-09',true),
+(2,'DOLCE GABBANA K','Pháp','Nữ','Hoa hồng','1 tháng',300000,'anh3.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-09-09',true),
+(2,'DOLCE GABBANA K EDP','Pháp','Nữ','Hoa hồng','1 tháng',700000,'anh4.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2022-02-01',true),
+(2,'LIGHT BLUE EAU INTENSE WOMEN','Pháp','Nữ','Hoa hồng','1 tháng',750000,'anh5.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-12-09',true),
+(2,'DOLCE ROSA EXCELCA EDP','Pháp','Nữ','Hoa hồng','1 tháng',1000000,'anh6.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-07-09',true),
+(2,'THE ONE FOR MEN EDP','Pháp','Nữ','Hoa hồng','1 tháng',1200000,'anh7.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-08-09',true),
+(2,'LIGHT BLUE POUR HOMME EDT','Pháp','Nữ','Hoa hồng','1 tháng',1500000,'anh8.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-01-09',true),
+(2,'DOLCE SHINE','Pháp','Nữ','Hoa hồng','1 tháng',2000000,'anh9.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-11-09',true),
+(2,'DOLCE GABBANA FLOWER EDP','Pháp','Nữ','Hoa hồng','1 tháng',2200000,'anh10.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-11-09',true),
 
+(4,'DOLCE GABBANA K EDP 100ML','Pháp','Unisex','Hoa hồng','1 tháng',500000,'anh1.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-11-09',true),
+(4,'DOLCE GABBANA THE ONE FOR MEN EDT 150ML','Pháp','Unisex','Hoa hồng','1 tháng',350000,'anh2.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-10-09',true),
+(4,'DOLCE GABBANA K','Pháp','Unisex','Hoa hồng','1 tháng',300000,'anh3.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-09-09',true),
+(4,'DOLCE GABBANA K EDP','Pháp','Unisex','Hoa hồng','1 tháng',700000,'anh4.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2022-02-01',true),
+(4,'LIGHT BLUE EAU INTENSE WOMEN','Pháp','Unisex','Hoa hồng','1 tháng',750000,'anh5.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-12-09',true),
+(4,'DOLCE ROSA EXCELCA EDP','Pháp','Unisex','Hoa hồng','1 tháng',1000000,'anh6.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-07-09',true),
+(4,'THE ONE FOR MEN EDP','Pháp','Unisex','Hoa hồng','1 tháng',1200000,'anh7.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-08-09',true),
+(4,'LIGHT BLUE POUR HOMME EDT','Pháp','Unisex','Hoa hồng','1 tháng',1500000,'anh8.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-01-09',true),
+(4,'DOLCE SHINE','Pháp','Unisex','Hoa hồng','1 tháng',2000000,'anh9.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-11-09',true),
+(4,'DOLCE GABBANA FLOWER EDP','Pháp','Unisex','Hoa hồng','1 tháng',2200000,'anh10.jpg','Nước hoa hương thơm ngào ngạt','dfasdfasdfasfdasfdasfdasfdasfasfasfdasfasfasfasfasfasfasfasfasfasfdasf',100,'2021-11-09',true);
 
 -- insert data introduce
 select * from introduce;
@@ -274,6 +278,15 @@ insert into cart values
 (2,29,2);
 
 
+
+
+----------------------------------------------------------------------------
+SET SQL_SAFE_UPDATES = 0;
+delete from bill_detail;
+delete from bill;
+delete from cart;
+delete from product;
+delete from brand;
 --------------------------------------------------------------------------------------------------------------------
 SET SQL_SAFE_UPDATES = 0;
 update product 
